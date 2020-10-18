c6268561001fb388129067a6000badaabba3e2c9
cesar zamora martinez <cesar_czm@ciencias.unam.mx>
2020-10-17 20:49:52 -0500

    Primeras notas y scripts

---
+++ Scripts/00_introduction.R
@@ -0,0 +1,15 @@
+#Script de ejemplo de R
+
+## Imprimiendo mensaje :D
+print("Este es mi primer ejemplo")
+
+## Arimetica
+print("Aritmetica")
+15+6
+20-10
+5**3
+log(10000)
+
+## Asignamos variables
+n <- 10
+n+20
